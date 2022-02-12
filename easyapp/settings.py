0dbcb24eb33dd833c02fc12a4ec9bf5cd4dab49e
Rj12-art <rashj40@gmail.com>
2022-02-12 13:28:32 +0000

    Made some adjustments on the main nav

--- easyapp/settings.py
+++ easyapp/settings.py
@@ -23,10 +23,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'ztu5dhnioolk=k_oa7ga8p+v8eo1&&hk@x8hxn60wu)g*l4n*0'
 
-#SECRET_KEY = 'ztu5dhnioolk=k_oa7ga8p+v8eo1&&hk@x8hxn60wu)g*l4n*0'
-
-#SECRET_KEY = 'ztu5dhnioolk=k_oa7ga8p+v8eo1&&hk@x8hxn60wu)g*l4n*0'
-
 #SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
